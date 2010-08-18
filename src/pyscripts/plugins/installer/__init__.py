6789eee4e0478ed068e7d32503320e06c9af9dde
Harry Delmolino <harry@sexbomb.local>
2010-08-18 01:00:37 -0400

    added stuffxc

--- src/pyscripts/plugins/installer/__init__.py
+++ src/pyscripts/plugins/installer/__init__.py
@@ -16,7 +16,8 @@ import urllib
 config = {
 	'Main': {
 		'enable': 'yes',
-		'repo': 'http://github.com/downloads/harrythedevman/XSBS-plugins/'
+		'repo': 'http://github.com/harrythedevman/XSBS-plugins/',
+		'list': 'http://github.com/harrythedevman/XSBS-plugins/blob/master/list'
 		}
 	}
 
@@ -25,43 +26,64 @@ class Install:
 		self.pluginList = []
 		self.pyscriptsDir = os.getcwd()
 		self.downloadDir = os.path.abspath(self.pyscriptsDir + "/plugins/installer/downloads")
-		
+		self.pluginsDir = os.path.abspath(self.pyscriptsDir + "/plugins")
+	
 	def getPluginList(self):
-		file = open(self.fetch(config['Main']['repo']), 'r')
-		self.pluginList = file.readlines()
-		for line in self.pluginList:
-			line.strip('\n')
+		path = self.download(config['Main']['list'])
+		file = open(path, 'r')
+		for line in file.readlines():
+			self.pluginList.append(line.strip('\n'))
+		os.remove(path)
 		
-	def fetch(self, name):
+	def download(self, url):
 		file = url.split('/')[-1]
-		urllib.urlretrieve(url, self.downloadDir + file)
-		return os.path.abspath(self.downloadDir + file)
+		urllib.urlretrieve(url, self.downloadDir + "/" + file)
+		return os.path.abspath(self.downloadDir + "/" + file)
+	
+	def fetch(self, name):
+		return self.download(config['Main']['repo'] + name + ".tar.gz")
 		
-	def install(self, Name):
-		self
-		
+	def install(self, name):
+		file = self.fetch(name)
+		tf = tarfile.open(file)
+		tf.extractall(self.downloadDir + "/" + name)
+		installFile = self.downloadDir + "/" + name + "/install"
+		moves = parseINSTALL(installFile)
+		for k, v in moves.iteritems():
+			os.rename(self.downloadDir + "/" + name + "/" + k , v + k)
+			
 	def parseINSTALL(self, file):
-		self.paths = {'plugin_dir': self.pyscriptsDir + "/plugins"}	
-
+		paths = {'plugin_dir': self.pluginsDir, 'pyscripts_dir': self.pyscriptsDir}
+		moves = {}
+		for line in file.readlines():
+			line = string.Template(line)
+			line = line.substitute(paths)
+			line = line.split(':')
+			moves[line[0]] = line[1]
+		return moves
+			
 @commandHandler('install')
 @adminRequired
 def onInstallCmd(cn, args):
 	'''@description Install server plugins
 	   @usage <plugin>
 	   @admin'''
-	p = player(cn)
+	#p = player(cn)
 	plugin = args
 	installer = Install()
+	installer.getPluginList()
 	if plugin in installer.pluginList:
-		p.message(info("Plugin found. Downloading now."))
-		installer.download(plugin)
-		p.message(info("Plugin downloaded. Installing now."))
+		#p.message(info("Plugin found. Downloading now."))
+		print 1
+		installer.fetch(plugin)
+		#p.message(info("Plugin downloaded. Installing now."))
+		print 2
 		installer.install(plugin)
-		p.message(info("Plugin Installed."))
+		#p.message(info("Plugin Installed."))
+		print 3
 	else:
-		p.message(info("Plugin \"%s\" not found." % plugin))
-	
-	return
+		#p.message(info("Plugin \"%s\" not found." % plugin))
+		return
 	   
 def init():
 	print os.getcwd()
